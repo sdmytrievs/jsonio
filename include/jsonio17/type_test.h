1262cb9961f94923a72c1e4c9a7b024ef6e0a693
svetad <sv.dmytr@gmail.com>
2020-12-23 20:09:32 +0200

    Before merge

--- include/jsonio17/type_test.h
+++ include/jsonio17/type_test.h
@@ -17,8 +17,8 @@ struct is_mappish_impl : std::false_type { };
 
 template<typename T>
 struct is_mappish_impl<T, void_t<typename T::key_type,
-                                 typename T::mapped_type,
-                                 decltype(std::declval<T&>()[std::declval<const typename T::key_type&>()])>>
+        typename T::mapped_type,
+        decltype(std::declval<T&>()[std::declval<const typename T::key_type&>()])>>
                                                                                     : std::true_type { };
 
 
@@ -29,10 +29,10 @@ template<typename T>
 struct is_container_impl<T, void_t<typename T::value_type,
         typename T::mapped_type,
         decltype(std::declval<T>().size()),
-        decltype(std::declval<T>().begin()),
-        decltype(std::declval<T>().end()),
-        decltype(std::declval<T>().cbegin()),
-        decltype(std::declval<T>().cend())>>
+decltype(std::declval<T>().begin()),
+decltype(std::declval<T>().end()),
+decltype(std::declval<T>().cbegin()),
+decltype(std::declval<T>().cend())>>
                                      : std::true_type { };
 }
 
@@ -47,59 +47,59 @@ struct is_mappish : detail::is_mappish_impl<T>::type { };
 // http://fuelcycle.org/cyclus/api/prettyprint_8hpp_source.html
 
 template <bool, typename S, typename T> struct conditional { };
-  template <typename S, typename T> struct conditional<true,  S, T> { typedef S type; };
-  template <typename S, typename T> struct conditional<false, S, T> { typedef T type; };
+template <typename S, typename T> struct conditional<true,  S, T> { typedef S type; };
+template <typename S, typename T> struct conditional<false, S, T> { typedef T type; };
 
-  template <bool, typename T> struct enable_if { };
-  template <typename T> struct enable_if<true, T> { typedef T type; };
+template <bool, typename T> struct enable_if { };
+template <typename T> struct enable_if<true, T> { typedef T type; };
 
 
 // SFINAE type trait to detect whether T::const_iterator exists.
 
-     template<typename T>
-     struct has_const_iterator
-     {
-     private:
-         typedef char                      yes;
-         typedef struct { char array[2]; } no;
+template<typename T>
+struct has_const_iterator
+{
+private:
+    typedef char                      yes;
+    typedef struct { char array[2]; } no;
 
-         template<typename C> static yes test(typename C::const_iterator*);
-         template<typename C> static no  test(...);
-     public:
-         static const bool value = sizeof(test<T>(0)) == sizeof(yes);
-         typedef T type;
-     };
+    template<typename C> static yes test(typename C::const_iterator*);
+    template<typename C> static no  test(...);
+public:
+    static const bool value = sizeof(test<T>(0)) == sizeof(yes);
+    typedef T type;
+};
 
-     // SFINAE type trait to detect whether "T::const_iterator T::begin/end() const" exist.
+// SFINAE type trait to detect whether "T::const_iterator T::begin/end() const" exist.
 
-     template <typename T>
-     struct has_begin_end
-     {
-         struct Dummy { typedef void const_iterator; };
-         typedef typename conditional<has_const_iterator<T>::value, T, Dummy>::type TType;
-         typedef typename TType::const_iterator iter;
+template <typename T>
+struct has_begin_end
+{
+    struct Dummy { typedef void const_iterator; };
+    typedef typename conditional<has_const_iterator<T>::value, T, Dummy>::type TType;
+    typedef typename TType::const_iterator iter;
 
-         struct Fallback { iter begin() const; iter end() const; };
-         struct Derived : TType, Fallback { };
+    struct Fallback { iter begin() const; iter end() const; };
+    struct Derived : TType, Fallback { };
 
-         template<typename C, C> struct ChT;
+    template<typename C, C> struct ChT;
 
-         template<typename C> static char (&f(ChT<iter (Fallback::*)() const, &C::begin>*))[1];
-         template<typename C> static char (&f(...))[2];
-         template<typename C> static char (&g(ChT<iter (Fallback::*)() const, &C::end>*))[1];
-         template<typename C> static char (&g(...))[2];
+    template<typename C> static char (&f(ChT<iter (Fallback::*)() const, &C::begin>*))[1];
+    template<typename C> static char (&f(...))[2];
+    template<typename C> static char (&g(ChT<iter (Fallback::*)() const, &C::end>*))[1];
+    template<typename C> static char (&g(...))[2];
 
-         static bool const beg_value = sizeof(f<Derived>(0)) == 2;
-         static bool const end_value = sizeof(g<Derived>(0)) == 2;
-     };
+    static bool const beg_value = sizeof(f<Derived>(0)) == 2;
+    static bool const end_value = sizeof(g<Derived>(0)) == 2;
+};
 
-     // Basic is_container template; specialize to have value "true" for all desired container types
+// Basic is_container template; specialize to have value "true" for all desired container types
 
 //     template<typename T> struct is_container { static const bool value = has_const_iterator<T>::value && has_begin_end<T>::beg_value && has_begin_end<T>::end_value; };
 
 template<typename T>
- struct is_container : std::integral_constant<bool, has_const_iterator<T>::value && has_begin_end<T>::beg_value && has_begin_end<T>::end_value>
- { };
+struct is_container : std::integral_constant<bool, has_const_iterator<T>::value && has_begin_end<T>::beg_value && has_begin_end<T>::end_value>
+{ };
 
 //     template<typename T, std::size_t N> struct is_container<T[N]> { static const bool value = true; };
 
@@ -145,7 +145,6 @@ template<typename T>
  struct is_container : std::integral_constant<bool, has_const_iterator<T>::value && has_begin_end<T>::beg_value && has_begin_end<T>::end_value>
  { };
 */
-
 
 
 // other
