22512d830bd3c9b75514bf7937523abb2803f5bb
svetad <sv.dmytr@gmail.com>
2021-03-31 10:48:25 +0300

    No inline exceptions

--- src/exceptions.cpp
+++ src/exceptions.cpp
@@ -1,2 +1,12 @@
 #include "jsonio17/exceptions.h"
 
+
+void jsonio17::JSONIO_THROW(const std::string &title, int id, const std::string &message)
+{
+    throw jsonio_exception::create(title, id, message);
+}
+
+void jsonio17::JSONIO_THROW_IF(bool error, const std::string &title, int id, const std::string &message)
+{
+    if(error) throw jsonio_exception::create(title, id, message);
+}
