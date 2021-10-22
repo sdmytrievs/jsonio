09774b0bfb7dcb7d2aa5faf610bd12814ad6f49c
svetad <sv.dmytr@gmail.com>
2021-10-22 11:54:19 +0300

    Try fix conda

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -1,6 +1,7 @@
 conda config --system --set always_yes yes --set changeps1 no
 conda config --system --append channels conda-forge
 conda install -n base conda-devenv
+conda clean --all --yes
 conda update -q conda
 conda info -a
 conda devenv
