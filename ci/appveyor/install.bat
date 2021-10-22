746b1a54d27c775b716e19510bb7257a5fca0301
svetad <sv.dmytr@gmail.com>
2021-10-22 12:37:00 +0300

    Change unique_lock to lock_guard

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -1,6 +1,5 @@
 conda config --system --set always_yes yes --set changeps1 no
 conda config --system --append channels conda-forge
-conda clean --packages
 conda install -n base conda-devenv
 conda update -q conda
 conda info -a
